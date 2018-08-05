0bbdd15165807d86e86b8790a2dadaddc19a654f
Jochen Peters <dev@emirp.net>
2018-08-05 15:34:03 +0200

    rustdoc/sdl2sketch updated

---
+++ rustdoc/implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["sdl2sketch"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"sdl2sketch/enum.AngleMode.html\" title=\"enum sdl2sketch::AngleMode\">AngleMode</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"sdl2sketch/enum.RectMode.html\" title=\"enum sdl2sketch::RectMode\">RectMode</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"sdl2sketch/enum.ImageMode.html\" title=\"enum sdl2sketch::ImageMode\">ImageMode</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
