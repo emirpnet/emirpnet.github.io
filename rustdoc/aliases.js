bada297481e88a3e626ca0eacfb41045e42a5d62
Jochen Peters <dev@emirp.net>
2020-11-15 12:55:25 +0100

    rustdoc/sdl2ketch updated

---
+++ rustdoc/aliases.js
@@ -0,0 +1,2 @@
+var ALIASES = {};
+ALIASES["sdl2sketch"] = {};
---
+++ rustdoc/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["sdl2sketch"] = {"name":"","files":["lib.rs","utils.rs"]};
+createSourceSidebar();
